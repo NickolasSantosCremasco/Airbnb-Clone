d2bd1cdfcf4c05b5d589cb1a28ff4968ac7655db
Nickolas Dos Santos Cremasco <nickolascremasco@gmail.com>
2025-11-20 09:59:15 -0300

    menulink

--- app/components/Navbar/UserNav.tsx
+++ app/components/Navbar/UserNav.tsx
@@ -1,9 +1,17 @@
 'use client';
 
+import { useState } from "react";
+import MenuLink from "./MenuLink";
+
 const UserNav = () => {
+
+    const [isOpen, setIsOpen] = useState(true)
     return(
         <div className="p-2 relative inline-block border rounded-full">
-            <button className="cursor-pointer flex items-center">
+            <button 
+                onClick={() => setIsOpen(false)}
+                className="cursor-pointer flex items-center"
+            >
                 <svg  fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
@@ -13,6 +21,20 @@ const UserNav = () => {
                 </svg>
 
             </button>
+
+            {isOpen && (
+                <div className="w-[220px] flex flex-col cursor-pointer absolute top-[60px] right-0 bg-white rounded-xl shadow-md">
+                    <MenuLink
+                        label="Log in"
+                        onClick={() => console.log('opa')}
+                    />
+
+                    <MenuLink
+                        label="Sign Up"
+                        onClick={() => console.log('opa')}
+                    />
+                </div>
+            )}
         </div>
     )
 }
